60735d2e8d343660806ce64536bf3334d0326b52
CodeChinh <Quyle552@gmail.com>
2022-11-17 07:43:42 +0700

    baxiu

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: " Thế có yêu Bá Xíu không? :v ",
+  descriptionHeader:
+    "Nếu thoát ra và không trả lời thì có nghĩa là ' CÓ ' rồi nhé :v",
+  buttonYes: "Có",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: " Hehe ! Thế là có nhé",
+  descriptionModar:
+    'Há há </br> :v :v :v </br>  Lêu lêu </br>  :v :v :v ',
+};
